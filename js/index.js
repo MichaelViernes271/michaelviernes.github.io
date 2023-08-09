375a5ec57e1e25fa889ec56c309c8a6d143459de
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2023-08-09 17:52:33 +0800

    preparing the content

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ const darkMode = document.querySelector('.switch');
 const navLinks = document.querySelectorAll('.nav__link');
 
 // create a button for dark mode
-darkMode.addEventListener('click', ()=> {
+/*darkMode.addEventListener('click', ()=> {
     document.body.classList.toggle('.dark-mode');
-})
+})*/
 
 navToggle.addEventListener('click', ()=> {
     document.body.classList.toggle('nav-open');
