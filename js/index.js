af62e03d4674bc5b8203c6abaa3baf5493f984c4
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2023-08-09 16:38:32 +0800

    completed the template for my portfolio items

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const navToggle = document.querySelector('.nav-toggle');
+const darkMode = document.querySelector('.switch');
 const navLinks = document.querySelectorAll('.nav__link');
 
 // create a button for dark mode
-/*navToggle.addEventListener('click', ()=> {
-    document.body.classList.toggle('dark-mode');
-})*/
+darkMode.addEventListener('click', ()=> {
+    document.body.classList.toggle('.dark-mode');
+})
 
 navToggle.addEventListener('click', ()=> {
     document.body.classList.toggle('nav-open');
